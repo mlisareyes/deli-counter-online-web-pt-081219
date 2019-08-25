0f6d1ce71fd67af55606da27beaf6b7b70fd9253
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-25 16:42:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(array, name)
   array << name
   if array.length == 1
-    "Welcome, #{name}. You are number 1 in line."
+    puts "Welcome, #{name}. You are number 1 in line."
   else
     hello
   end
