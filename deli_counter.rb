90a95d4ecb80c868475241f51b09729df63e62c0
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-25 16:27:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    current_line = []
+    array.each_with_index { |name, index| current_line << "#{index + 1}. #{name}"}
+    puts "The line is currently: #{current_line.join(" ")}"
+    #create a new array with properly numbered names
+    #use the shovel method to put the new names into the array
+    #use string interpolation to show each array element in order seperated by a space
+  end
+end
