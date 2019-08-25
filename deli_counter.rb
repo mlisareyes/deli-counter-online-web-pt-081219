699ffa937e5a985d43e86f6149bd12197b0d9955
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-25 17:07:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{name}[0]"
+    puts "Currently serving #{name[0]}"
     array.pop
   end
 end
